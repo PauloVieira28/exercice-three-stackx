5c1175a3873a36f2d5a6daf49803248252f7807c
Paulo Cesar <paulo_vieira22@hotmail.com>
2023-06-08 15:21:05 -0300

    Update highorder second tentative.js

--- highorder.js
+++ highorder.js
@@ -1,20 +1,20 @@
 // Implemente a função 'dobrarNumeros' que recebe um array de números
 // e retorna um novo array com cada número dobrado.
-function dobrarNumeros(numeros) {
+function dobrarNumeros(array) {
     // Sua implementação aqui
     return array.filter(num => num % 2 === 0);
 }
 
 // Implemente a função 'somarValores' que recebe um array de números
 // e retorna a soma de todos os valores.
-function somarValores(numeros) {
+function somarValores(array) {
     // Sua implementação aqui
     return array.reduce((total, num) => total + num, 0);
 }
 
 // Implemente a função 'filtrarPares' que recebe um array de números
 // e retorna um novo array contendo apenas os números pares.
-function filtrarPares(numeros) {
+function filtrarPares(array) {
     // Sua implementação aqui
     return array.map(num => num * 2);
 }
